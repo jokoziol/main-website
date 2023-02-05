14c6e1957179ef3baa1236d546aef23fbaed9bdd
Jonas <100228741+jokoziol@users.noreply.github.com>
2023-02-05 19:09:29 +0100

    Inital commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const text = "./init";
+
+let intervalId, terminalInitIntervalId;
+
+let count = 0
+
+function display(){
+
+    const element = document.getElementById("terminalRowOne")
+    const elementTextLength = element.innerText.length
+
+    element.innerText += text.charAt(elementTextLength)
+
+    if ((elementTextLength + 1) === text.length){
+        clearInterval(intervalId)
+
+        setTimeout(() => {
+            document.getElementById("terminalRowTwo").innerText = " "
+        }, 300)
+
+        setTimeout(() => {
+            terminalInitIntervalId = setInterval(() => terminalInitOutput(), 10)
+        }, 500)
+    }
+}
+
+function terminalInitOutput(){
+
+    count ++
+    document.getElementById("terminalRowTwo").innerText = "[" + count + "%]"
+
+    if (count >= 100){
+        clearInterval(terminalInitIntervalId)
+
+        setTimeout(() => {
+
+            let link = document.createElement("a")
+            link.href = "https://github.com/jokoziol"
+            link.innerHTML = link.href
+
+            document.getElementById("terminalRowTwo").innerText += "\n\n=> GitHub: "
+            document.getElementById("terminalRowTwo").appendChild(link)
+        }, 200)
+
+        setTimeout(() => {
+            document.getElementById("terminalEndText").style.display = "inline"
+
+            setInterval(() => terminalCursor(), 150)
+        }, 200)
+    }
+}
+
+function terminalCursor(){
+    const element = document.getElementById("terminalEndText")
+    if (element.hasAttribute("underscore")){
+        element.removeAttribute("underscore")
+        element.innerText = "root@localhost:~$"
+    }else{
+        element.setAttribute("underscore", true)
+        element.innerText = "root@localhost:~$ _"
+    }
+}
+
+function init(){
+    intervalId = setInterval(() => display(), 150)
+}
