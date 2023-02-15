9a3c239de6018511544de82d3853a8517217b909
Jonas <100228741+jokoziol@users.noreply.github.com>
2023-02-15 07:37:31 +0100

    Added scripts

---
+++ scripts/terminal.js
@@ -0,0 +1,149 @@
+function addKeydownEventListener(){
+    document.addEventListener("keydown", keyPressed)
+}
+
+function removeKeydownEventListener(){
+    document.removeEventListener("keydown", keyPressed)
+}
+
+function keyPressed(event){
+
+    if(event.defaultPrevented){
+        return
+    }
+
+    const element = document.getElementById("commandInput")
+
+    if (event.key.match(/(^\w{1}$)|\s{1}/)){
+        element.innerText += event.key
+    }else if (event.key === "Backspace"){
+        element.innerText = element.innerText.slice(0, -1)
+    }else if (event.key === "Enter"){
+        executeCommand(element.innerText)
+    }
+
+    event.preventDefault()
+}
+
+function executeCommand(command){
+
+    removeKeydownEventListener()
+    clearInterval(terminalCursorIntervalId)
+    document.getElementById("terminalCursor").remove()
+    document.getElementById("commandInput").removeAttribute("id")
+
+    const parentElement = document.getElementById("terminalWindow")
+
+    if (command !== "clear"){
+        const div = document.createElement("div")
+        const terminalText = document.createElement("p")
+
+        terminalText.classList.add("terminalText")
+        terminalText.innerText = getOutput(command.split(" ")[0], command.substring(command.indexOf(" ") + 1))
+
+        div.appendChild(terminalText)
+        parentElement.appendChild(div)
+    }else{
+        parentElement.replaceChildren()
+    }
+
+    const secondDiv = document.createElement("div")
+    const terminalLeadingElement = document.createElement("p")
+    terminalLeadingElement.classList.add("terminalLeadingText")
+    terminalLeadingElement.innerText = "root@localhost:~$ "
+
+    const commandInputElement = document.createElement("p")
+    commandInputElement.classList.add("terminalText")
+    commandInputElement.id = "commandInput"
+
+    const terminalCursor = document.createElement("p")
+    terminalCursor.classList.add("terminalLeadingText")
+    terminalCursor.id = "terminalCursor"
+    terminalCursor.style.display = "inline"
+    terminalCursor.innerText = "_"
+
+    if(command !== "clear"){
+        const emptyElement = document.createElement("p")
+        secondDiv.appendChild(emptyElement)
+    }
+
+    secondDiv.appendChild(terminalLeadingElement)
+    secondDiv.appendChild(commandInputElement)
+    secondDiv.appendChild(terminalCursor)
+    parentElement.appendChild(secondDiv)
+
+    addKeydownEventListener()
+    terminalCursorIntervalId = setInterval(() => toggleTerminalCursor(), 150)
+
+    window.scrollTo(0, document.body.scrollHeight)
+}
+
+function getOutput(command, args){
+
+    const allCommands = getAllCommands(args)
+
+    for (const [key, value] of Object.entries(allCommands)){
+
+        if(command === allCommands[key].key){
+            return value.value
+        }
+    }
+
+    return "The command \"" + command + "\" could not be found"
+}
+
+function getAllCommands(args){
+
+    let argsArray = args.length === 0 ? [] : args.split(" ")
+
+    return [{
+        key: "help",
+        value: args.length === 0 ? "" : getHelp()
+    },{
+        key: "clear",
+        value: "",
+        description: "Clears the output"
+    },{
+        key: "echo",
+        value: args,
+        description: "Outputs a text",
+        argsDescription: "[text]"
+    },{
+        key: "generateNumber",
+        value: getRandomNumber(argsArray[0], argsArray[1]),
+        description: "Generates a number between the specified number range",
+        argsDescription: "min max"
+    }]
+}
+
+function getRandomNumber(min, max){
+
+    if(min > max){
+        return 0
+    }
+
+    if(min != parseInt(min) || max != parseInt(max)){
+        return 0
+    }
+
+    return Math.floor(Math.random() * (max - min) + max)
+}
+
+function getHelp(){
+
+    const allCommands = getAllCommands("")
+    let helpResult = "----------------"
+
+    for(const [key, value] of Object.entries(allCommands)){
+
+        if ("description" in allCommands[key]){
+            helpResult += "\n\n- " + value.key + ": " + value.description
+        }
+
+        if("argsDescription" in allCommands[key]){
+            helpResult += "\n=> Arguments: " + value.argsDescription
+        }
+    }
+
+    return helpResult + "\n\n-----------------"
+}
